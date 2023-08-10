6eaf885502b92c7d336fa80eb69c757bd5dcd9a9
Kadiraydn01 <kadiraydn01@gmail.com>
2023-08-11 00:18:46 +0300

    Başlangıç Yapıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
+import Home from "./Home";
+import "./App.css";
 
 const App = () => {
-  return (
-    <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
-    </>
-  );
+  return <Home />;
 };
 export default App;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div className="home-div">
+      <h3>Teknolojik Yemekler</h3>
+      <br />
+      <h2>KOD ACIKTIRIR</h2>
+      <h2>PİZZA DOYURUR</h2>
+      <button className="btn">ACIKTIM</button>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/91/Pizza-3007395.jpg/330px-Pizza-3007395.jpg"
+        alt="pizza"
+      ></img>
+    </div>
+  );
+};
+
+export default Home;
